91ea77ba5a71fa8395715784962028d292b4e4af
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-01-18 01:52:19 -0600

    finished assignment for conditonals

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ return numberFour + numberSeven;
 
 }
 console.log('Add Numbers', result);
+////////////////////////////////
+let numbers = (56, 250);
+if (56 > 250) {
+console.log ('The first number is bigger');
+
+} 
+else if (250 > 56){
+    console.log('The second number is bigger');
+
+}
+else  
+console.log ('The numbers are the same');
+
+let weight = (450, 6);
+if (450 > 6) {
+    console.log('The first number is bigger');
+}
+else if (450 < 6) {
+    console.log("The Second number is bigger");
+}
+else  
+    console.log('The numbers are the same!');
+
+let height = (208, 208);
+if (208 > 208){
+    console.log ('the first number is higher');
+}
+    else if (208 < 208) {
+        console.log (' The second number is bigger');
+        }
+        else 
+            console.log('The numbers are the same!');
+
+        
+
+    
+
